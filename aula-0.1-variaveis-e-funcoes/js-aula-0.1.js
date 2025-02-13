b242ead35e57fe72d6292f7806fa24ff4445a9b7
1filipeolv <filipeolv.lima@gmail.com>
2025-02-13 18:55:23 -0300

    Variáveis e funções no JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,19 @@
+// Comentário em linha JS
+/* Comentário
+em bloco ... */
+// CTRL + ; - Atalho
+
+// Declaração de variáveis no JS
+// CONST X LET X VAR
+// Por questões de segurança evitar o uso de VAR
+// No escopo global CONST não pode ser alterado
+const nome = "Filipe" // Uma constante precisa de um valor
+let Nome // Permite iniciar a variável vazia
+// const nome ="Pedro" -> Resulta em um erro!
+// let Nome -> Resulta em um erro.
+var cor ="azul"
+let cidade = "Registro"
+cidade ="Jacupiranga" // Let permite alterar o valor.
+const message = "Hello, World! Iniciando estudos com Javascript" 
+
+console.log(message)
